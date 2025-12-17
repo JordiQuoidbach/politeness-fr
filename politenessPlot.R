8ac517da44e42edaac541ade2d6f4830ba4ca002
Jordi Quoidbach <jordiquoidbach@MacBook-Pro-9.local>
2025-12-17 11:19:18 +0100

    Update documentation and verify all tests pass
    - All 92 tests passing (57 French-specific tests)
    - Comprehensive functionality checks verified
    - All new features (Imparfait.Politesse, Conditional.Politeness) working correctly
    - Package structure validated

---
+++ politenessPlot.R
@@ -0,0 +1,24 @@
+#' Politeness plot
+#'
+#' @description Deprecated... This function has a new name now. See featurePlot for details.
+#' @param df_polite a data.frame with politeness features calculated from a document set, as output by \code{\link{politeness}}.
+#' @param ... other arguments passed on to featurePlot. See featurePlot for details.
+#' @return a ggplot of the prevalence of politeness features, conditional on \code{split}. Features are sorted by variance-weighted log odds ratio.
+#' @examples
+#'
+#' data("phone_offers")
+#'
+#' polite.data<-politeness(phone_offers$message, parser="none", drop_blank=FALSE)
+#'
+#' politeness::politenessPlot(polite.data,
+#'                            split=phone_offers$condition,
+#'                            split_levels = c("Tough","Warm"),
+#'                            split_name = "Condition",
+#'                            top_title = "Average Feature Counts")
+#'
+#' @export
+
+politenessPlot <- function(df_polite, ...) {
+  .Deprecated("featurePlot")
+  featurePlot(df_polite, ...)
+}
